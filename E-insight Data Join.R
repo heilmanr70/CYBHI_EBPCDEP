a1e5da75db2f2b07ea72dcd4bf9e8d4d0aebe2f3
tburgess82 <tristan_burgess@yahoo.com.au>
2025-04-30 22:42:54 +1000

    Been working on this

--- E-insight Data Join.R
+++ E-insight Data Join.R
@@ -1,29 +1,36 @@
 library(openxlsx)
+library(dplyr)
+
 
 discharge.data <- read.xlsx("/Users/tristan/CWS Dropbox/Tristan Burgess/CYBHI Project/Rebecca and Tristan and Cristin/EInsight Data/Exports - Raw/2025_04/CYBHI_EInsights Export_Discharge Summary Report_Export 04.07.2025.xlsx")
 #outcomes.data <- read_xlsx("~/Dropbox/Rebecca and Tristan and Cristin/EInsight Data/Exports - Raw/2025_04/Discharge/CYBHI_EInsights Export_Discharge Summary Report_Export 02.14.2025.xlsx")
 
+# [TB] NOTE - AS FAR AS I CAN TELL, THE ONLY ADDITIONAL USEFUL PIECE OF DATA IN THIS FILE IS "COMPLETED" - PRESUMABLY THAT IS MODEL COMPLETION?
 clinical.data <- read.xlsx("/Users/tristan/CWS Dropbox/Tristan Burgess/CYBHI Project/Rebecca and Tristan and Cristin/EInsight Data/Exports - Raw/2025_04/EInsight_Clinical Summary_04.07.2025.xlsx")
 
 demo.data <- read.xlsx("/Users/tristan/CWS Dropbox/Tristan Burgess/CYBHI Project/Rebecca and Tristan and Cristin/EInsight Data/Exports - Raw/2025_04/EInsight_Demographics_04.07.2025.xlsx")
-#demo.data <- read_xlsx("~/Dropbox/Rebecca and Tristan and Cristin/EInsight Data/Exports - Raw/2025_02/Discharge/MOCKUP CYBHI_EInsights Export_Demographics Summary Report_Export 02.14.2025.xlsx")
 
-join.data <- merge(outcomes.data,demo.data, by="Client ID")
+# Detect and remove entries where the Provide ID contains the text "DEMO" as these are not real data
+text <- demo.data$Provider.ID
+contains_DEMO <- grepl("DEMO", text)
+print(contains_DEMO)
+demo.data <- demo.data[which(!contains_DEMO),]
 
-length(outcomes.data$`Client ID`)
-length(demo.data$`Client ID`)
 
-test <- join.data$TestNum/join.data$`Client ID`
-max(test)
-min(test)
+
+# Data Joining
+join.data <- merge(discharge.data,demo.data, by="Client.ID",all.x=TRUE)
+length(discharge.data$`Client.ID`)
+length(demo.data$`Client.ID`)
+length(join.data$`Client.ID`)
+# [TB] 7 additional rows indicate that 7 clients had multiple entries in demo.data. Will need to deal with these once the reason is determined. Could be mistakes, but they seem quite consistent. Suspect that same client ID is being used for multiple members of a family group. All instances of multiple entries in the demo data sheet share the same 11 providers.
 
 #are there unique client IDs? 
+# [TB] Test fails in part becuase this is assessment-level not client level
 length(unique(join.data$`Client ID`)) == nrow(join.data)
 
 #which IDs have been repeated and how many times? 
 join.data %>% 
-  group_by(`Client ID`) %>% 
+  group_by(`Client.ID`) %>% 
   summarise(n=n()) %>% 
   filter(n>1)
-
-#NOW we have this!
